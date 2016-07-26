884e4c528878d2cf07b6d713bddcf1b0858e2b4e
Randall Spencer <randallspencer@gmail.com>
2016-07-26 13:51:11 -0700

    Add project files.

---
+++ PieceMeal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PieceMeal.Startup))]
+namespace PieceMeal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
